e0f98653cf4ecd628fb5a9401dfb0ad697d60a03
scottkelly36 <scottkelly36@gmail.com>
2020-12-07 20:35:34 +0000

    add messenger

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,21 +15,25 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap" rel="stylesheet">
-        
+
         <!--bootstrap css-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <!--font awsome-->
-        <script src="https://kit.fontawesome.com/889f0829fa.js"></script>       
+        <script src="https://kit.fontawesome.com/889f0829fa.js"></script>
     </head>
     <body>
         @include('layouts.navbar')
         @include('inc.messages')
         <div class="container-fluid" id="main">
-                
+
             @yield('content')
         </div>
+
+        <div class="fb-customerchat"
+        page_id="106901490710305">
+       </div>
     </body>
-    
+
     <!--bootstrap js-->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -49,13 +53,13 @@
         /* Open fullscreen overlay*/
         function openOverlay() {
             document.getElementById("full-overlay").style.width = "100%";
-           
+
         }
 
         /* Close fullscreen overlay */
         function closeOverlay() {
             document.getElementById("full-overlay").style.width = "0%";
-            
+
         }
     </script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
